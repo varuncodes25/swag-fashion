de11dce3a90258f4698a93088aacf37b42199352
varuncodes25 <varuntare2@gmail.com>
2025-07-28 21:23:44 +0530

    new bug fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -143,7 +143,7 @@ const forgotPassword = async (req, res) => {
     expiresIn: "15m",
   });
 
-  const resetLink = `https://swag-fashion-bcah.vercel.app/${token}`;
+  const resetLink = `https://swag-fashion-bcah.vercel.app/reset-password/${token}`;
   const html = `<p>Click below to reset your password:</p><a href="${resetLink}">Reset Password</a>`;
 
   await sendMail(user.email, "Password Reset", html);
