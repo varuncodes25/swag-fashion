641b0eb3ad7d74d59b59a3911fcb33184e3f3e48
varuncodes25 <varuntare2@gmail.com>
2025-08-07 13:34:35 +0530

    bug fix

--- client/src/components/custom/HeaderDisplay.jsx
+++ client/src/components/custom/HeaderDisplay.jsx
@@ -9,7 +9,7 @@ import {
 
 import banner1 from "../../assets/banner.png";
 import banner2 from "../../assets/banner2.png";
-import banner3 from "../../assets/banner2.png";
+import banner3 from "../../assets/banner3.png";
 
 const HeaderDisplay = () => {
   const imagesData = [banner1, banner2, banner3];
