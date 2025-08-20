4558a79efc69d9b76ca9af36ad6ed8d8058e0145
varuncodes25 <varuntare2@gmail.com>
2025-08-20 18:51:02 +0530

    bug fix

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -7,7 +7,7 @@ var cartController = require("../controllers/cartController");
 router.post("/add", cartController.addToCart);
 
 // Get user's cart
-router.get("/:userId", cartController.getCart);
+router.get("/cart/:userId", cartController.getCart);
 
 // Remove product from cart
 router.post("/remove", cartController.removeFromCart);
