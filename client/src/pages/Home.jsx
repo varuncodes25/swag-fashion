df426a291b71a3512a521bc10f307c66ce88ed62
satyam70288 <satyamb971@gmail.com>
2026-02-15 22:51:01 +0530

    search functinality bug fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,17 @@ import FilterMenu from "@/components/custom/FilterMenu";
 import Banner from "@/components/custom/banner";
 import ProductList from "@/components/custom/ProductList";
 import HomeCollections from "@/components/Home/HomeCollection";
+import { useState } from "react";
 
 const Home = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <div>
       <Banner />
       <HomeCollections />
-      <FilterMenu />
-      <ProductList />
+      <FilterMenu onSearch={setSearch} />
+      <ProductList search={search} />
     </div>
   );
 };
