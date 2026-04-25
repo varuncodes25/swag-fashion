e2725168d6d7951bd3cf9854ff63fc8813825590
satyam70288 <satyamb971@gmail.com>
2026-04-26 01:13:31 +0530

    bug

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -27,7 +27,7 @@ exports.createCategory = async (req, res) => {
 // GET ALL CATEGORIES
 exports.getAllCategories = async (req, res) => {
   try {
-    const categories = await Category.find({ isActive: true });
+    const categories = await Category.find();
     res.json(categories);
   } catch (error) {
     res.status(500).json({ message: error.message });
